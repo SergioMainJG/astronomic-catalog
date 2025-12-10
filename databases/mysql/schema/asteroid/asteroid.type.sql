da8d850f3000565570dd8337e7d950c7d640e920
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-12-10 16:33:14 -0600

    modify: foreign-keys.sql -> better relationship, celestial-object.sql -> 3nf completed

--- databases/mysql/schema/asteroid/asteroid.type.sql
+++ databases/mysql/schema/asteroid/asteroid.type.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `AsteroidType`
 (
     `id`   INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(100),
+    `name` VARCHAR(100) NOT NULL,
     CONSTRAINT `check_asteroid_type_name` CHECK ( `name` REGEXP '^[A-Za-z0-9 .+'',-]+$' )
 
 );
--- databases/mysql/schema/foreign_keys.sql
+++ databases/mysql/schema/foreign_keys.sql
@@ -1,56 +1,59 @@
 ALTER TABLE `Galaxy`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `Star`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `Planet`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `NaturalSatellite`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `Asteroid`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `Comet`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `BlackHole`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `Nebula`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
-
 ALTER TABLE `StarCluster`
     ADD FOREIGN KEY (`id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `Galaxy`
     ADD FOREIGN KEY (`galaxy_type_id`) REFERENCES `GalaxyType` (`id`);
-
 ALTER TABLE `GalaxyArm`
     ADD FOREIGN KEY (`galaxy_id`) REFERENCES `Galaxy` (`id`);
-
 ALTER TABLE `StellarNeighborhood`
     ADD FOREIGN KEY (`arm_id`) REFERENCES `GalaxyArm` (`id`);
-
 ALTER TABLE `StarSystem`
     ADD FOREIGN KEY (`neighborhood_id`) REFERENCES `StellarNeighborhood` (`id`);
 
 ALTER TABLE `Star`
+    ADD FOREIGN KEY (`system_id`) REFERENCES `StarSystem` (`id`);
+
+
+ALTER TABLE `Star`
     ADD FOREIGN KEY (`spectral_type_id`) REFERENCES `StarSpectralType` (`id`);
+ALTER TABLE `Star`
+    ADD FOREIGN KEY (`constellation_id`) REFERENCES `Constellation` (`id`);
 
 ALTER TABLE `Planet`
     ADD FOREIGN KEY (`planet_type_id`) REFERENCES `PlanetType` (`id`);
-
 ALTER TABLE `Asteroid`
     ADD FOREIGN KEY (`type_id`) REFERENCES `AsteroidType` (`id`);
-
 ALTER TABLE `Comet`
     ADD FOREIGN KEY (`type_id`) REFERENCES `CometType` (`id`);
 
+
 ALTER TABLE `Nebula`
     ADD FOREIGN KEY (`neighborhood_id`) REFERENCES `StellarNeighborhood` (`id`);
-
 ALTER TABLE `StarCluster`
     ADD FOREIGN KEY (`galaxy_id`) REFERENCES `Galaxy` (`id`);
+
+ALTER TABLE `Orbit`
+    ADD FOREIGN KEY (`celestial_object_id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `Orbit`
+    ADD FOREIGN KEY (`primary_body_id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `Observation`
+    ADD FOREIGN KEY (`celestial_object_id`) REFERENCES `CelestialObject` (`id`) ON DELETE CASCADE;
