41409a7ba6e81057fc9c8a64107c9a552111841b
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-12-09 23:59:31 -0600

    add: 00-main.sh -> finally, starting the project correctly

--- databases/mysql/schema/black-hole/black-hole.sql
+++ databases/mysql/schema/black-hole/black-hole.sql
@@ -2,7 +2,5 @@ CREATE TABLE `BlackHole`
 (
     `id`         INT PRIMARY KEY,
     `mass_solar` DECIMAL(10, 4) NOT NULL,
-    `galaxy_id`  INT            NOT NULL,
-    `system_id`  INT            NOT NULL,
     CONSTRAINT `check_black_hole_mass_solar` CHECK ( `mass_solar` > 0.0000)
 );
--- databases/mysql/schema/planet/planet.sql
+++ databases/mysql/schema/planet/planet.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `Planet`
 (
     `id`             INT PRIMARY KEY,
-    `star_id`        INT,
     `planet_type_id` INT,
     `mass_earth`     DECIMAL(10, 4),
     `radius_earth`   DECIMAL(10, 4),
