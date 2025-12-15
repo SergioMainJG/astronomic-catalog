a60f639a99df367af4ec690f99c4ba5fb98dd47f
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-12-15 13:56:02 -0600

    modify triggers cause the calculus were wrong

--- databases/mysql/schema/triggers/validate-created_at.celestial-object.sql
+++ databases/mysql/schema/triggers/validate-created_at.celestial-object.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_celestial_object_before_insert
     ON CelestialObject
     FOR EACH ROW
 BEGIN
-    IF NEW.`created_at` > CURRENT_TIMESTAMP THEN
+    IF NEW.`created_at` > (CURRENT_TIMESTAMP + INTERVAL 1 MINUTE ) THEN
         SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'created_at can not be over the present time';
     END IF;
--- databases/mysql/schema/triggers/validate-updated_at.natural-satellite.sql
+++ databases/mysql/schema/triggers/validate-updated_at.natural-satellite.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_celestial_object_before_update
     ON CelestialObject
     FOR EACH ROW
 BEGIN
-    IF NEW.`updated_at` > CURRENT_TIMESTAMP THEN
+    IF NEW.`updated_at` > (CURRENT_TIMESTAMP + INTERVAL 1 MINUTE ) THEN
         SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'updated_at can not be over the present time';
     END IF;
